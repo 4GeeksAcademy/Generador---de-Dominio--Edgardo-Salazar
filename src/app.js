e66f30c689871a52eb6f96a645798acac1694fb0
EdgardoSalazar <82413057+EdgardoSalazar@users.noreply.github.com>
2025-10-20 19:15:53 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+  let pronoun = ['the', 'our'];
+  let adjevtive = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensiones = ['.com', '.net', '.us', '.io'];
+  // Contenedor donde se insertarán los dominios
+  let container = document.getElementById('domain-list');
+  // Generar combinaciones con bucles for
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjevtive.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensiones.length; l++) {
+          let domain = `${pronoun[i]}${adjevtive[j]}${noun[k]}${extensiones[l]}`;
+          console.log(domain);
+
+        }
+      }
+    }
+  }
+}
